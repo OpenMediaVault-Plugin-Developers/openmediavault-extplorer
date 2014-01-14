618ee4067cad38ea6b6000d028ac5ae4dac28d30
Aaron Murray <aaron@omv-extras.org>
2014-01-13 23:43:14 -0600

    change default password

--- var/www/extplorer/config/.htusers.php
+++ var/www/extplorer/config/.htusers.php
@@ -1,8 +1,7 @@
 <?php
-/** @version $Id: .htusers.php 135 2009-01-27 21:57:15Z ryu_ms $ */
-/** ensure this file is being included by a parent file */
-if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
-
-$GLOBALS["users"]=array(
-	array("admin","$2a$08$EIGV7Q1M8RE.vmsBd/WjY.g2sXri9CeAtevf7cOWSOdfGMy/uceCC",empty($_SERVER['DOCUMENT_ROOT'])?realpath(dirname(__FILE__).'/..'):$_SERVER['DOCUMENT_ROOT'],"http://localhost",1,"",7,1),
-); ?>
+// ensure this file is being included by a parent file
+        if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+        $GLOBALS["users"]=array(
+        array('admin','$2a$08$o9113Sa234xV8aGmC2mZvepUeYyPsUU5jibk3FxwP7nE7zTQ/S0Ha','/var/www/openmediavault','http://localhost','1','','7',1),
+);
+?>
