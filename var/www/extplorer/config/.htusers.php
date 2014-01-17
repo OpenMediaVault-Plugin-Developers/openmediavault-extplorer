2eb002c7d97d55e33fadebfd4b471b65fe0c1c10
Aaron Murray <aaron@omv-extras.org>
2014-01-17 09:36:05 -0600

    change admin user root directory

--- var/www/extplorer/config/.htusers.php
+++ var/www/extplorer/config/.htusers.php
@@ -2,6 +2,6 @@
 // ensure this file is being included by a parent file
         if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
         $GLOBALS["users"]=array(
-        array('admin','$2a$08$o9113Sa234xV8aGmC2mZvepUeYyPsUU5jibk3FxwP7nE7zTQ/S0Ha','/var/www/openmediavault','http://localhost','1','','7',1),
+        array('admin','$2a$08$o9113Sa234xV8aGmC2mZvepUeYyPsUU5jibk3FxwP7nE7zTQ/S0Ha','/','http://localhost','1','','7',1),
 );
 ?>
