173743bf0b4547561a0006f9160b00db9c1ab170
Aaron Murray <plugins@omv-extras.org>
2014-07-22 09:19:10 -0500

    download extplorer

--- usr/share/extplorer/config/.htusers.php
+++
@@ -1,7 +0,0 @@
-<?php
-// ensure this file is being included by a parent file
-        if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
-        $GLOBALS["users"]=array(
-        array('admin','$2a$08$o9113Sa234xV8aGmC2mZvepUeYyPsUU5jibk3FxwP7nE7zTQ/S0Ha','/','http://localhost','1','','7',1),
-);
-?>
